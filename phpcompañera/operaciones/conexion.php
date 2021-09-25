77311fc976e373f3d7f677051ff4e143c4bb28d7
Jonier-Ardila <82669815+Jonier-Ardila@users.noreply.github.com>
2021-09-25 01:30:08 -0500

    Create conexion.php

---
+++ phpcompañera/operaciones/conexion.php
@@ -0,0 +1,9 @@
+<?php
+$host="localhost";
+$username="root";
+$bdname="registro_php";
+//Permite realizar conexion con la base de datos creada.
+$conexion= mysqli_connect('localhost','root','','registro_php');
+?>
+
+ 
