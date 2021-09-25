931e6fabb61362b2db718b7fb66515503277f0c2
Jonier-Ardila <82669815+Jonier-Ardila@users.noreply.github.com>
2021-09-25 01:30:15 -0500

    Create index.php

---
+++ phpcompañera/operaciones/index.php
@@ -0,0 +1,32 @@
+<!DOCTYPE html>
+<html lang="es">
+<!--le indica al navegador que todo el contenido esta en español!-->
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Iniciar sesion Compensar</title>
+    <link rel="stylesheet" type="text/css" href="estilos.css">
+</head>
+<body>
+    <center>
+<form action="validar.php" method="post">
+        <h1>Ingresar los siguientes datos</h1>
+        <p>Nombre:<input type="text" placeholder="Ingrese su nombre" name="Nombre"></p>
+        <p>Apellido:<input type="text" placeholder="Ingrese su apellido" name="Apellido"></p>
+        <p>Edad:<input type="text" placeholder="Ingrese su edad" name="Edad"></p>
+        <p>Tipo de afiliacion:<input type="text" placeholder="Ingrese su tipo de afiliacion" name="Tipodeafiliacion"></p>
+        <p>Email:<input type="text" placeholder="Ingrese su email" name="Email"></p>
+        <input type="submit" value="Ingresar">
+        <h2>La mejor tienda, que piensa en ti</h2> 
+
+<!--Formulario para registrar los datos en la base de datos!-->
+    </form> 
+    <style>
+        body {
+          
+        }
+    
+    </style>
+</body>
+</html>
